298ca23ecb1f0eff389dd8b956948b999d03e2e6
akashmondal2000 <mondalakash494@gmail.com>
2026-06-12 20:48:31 +0530

    create a redux store , connect the store to the higher component, connect to the counter component, and dispatch the counter state, action

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,27 @@
-import classes from './Counter.module.css'; 
+import { useSelector, useDispatch  } from 'react-redux';
+import classes from './Counter.module.css';
+
 
 const Counter = () => {
+    const counter = useSelector( state => state.counter );
+    const dispatch = useDispatch();
+    const toggleCounterHandler = ()=>{};
 
-    const toggleCounterHandler = ()=>{
-
+    const incrementHandeler =()=>{
+        dispatch( { type: 'increment' })
     };
 
+    const decrementHandler =()=>{
+        dispatch( { type: "decrement"})
+    }
   return (
     <main className={classes.counter}>
         <h1>Counter App</h1>
-        <div className={classes.value}> Counter Value hare </div>
+        <div className={classes.value}> {counter}</div>
+        <div className={classes.buttons}>
+            <button onClick={incrementHandeler}>Increment</button>
+            <button onClick={decrementHandler}>Decrement</button>
+        </div>
         <button onClick={toggleCounterHandler} > Toggle Counter </button>
 
     </main>
