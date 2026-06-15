568dedb7d0bb2af6bd8bca43da564b1a7d1240ab
akashmondal2000 <mondalakash494@gmail.com>
2026-06-15 19:06:47 +0530

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Auth from "./components/Auth";
 import Counter from "./components/Counter";
+import Header from "./components/Header";
  
 const App = () => {
   return (
-    <Counter/>
+    <>
+      <Header/>
+      <Auth/>
+      <Counter/>
+    </>
   )
 }
 
